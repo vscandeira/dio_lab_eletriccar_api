4136f73c1515b0d641a4eb8819e2289ebc661a1b
Victor Santos Candeira <vscandeira@gmail.com>
2025-02-11 11:09:42 -0300

    first commit

---
+++ src/main/kotlin/com/example/eletriccar_api/EletriccarApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eletriccar_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EletriccarApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EletriccarApiApplication>(*args)
+}
